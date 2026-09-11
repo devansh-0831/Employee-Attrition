adccc5f8368174dc59a9d466bf30c34d19f4db20
devansh chandel <devanshchandelclg@gmail.com>
2026-09-11 18:45:38 +0530

    Update settings.py

--- employee_web/settings.py
+++ employee_web/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED-ORIGINS = ['https://hearty-empathy-production-b29f.up.railway.app',]
+CSRF_TRUSTED_ORIGINS = ['https://hearty-empathy-production-b29f.up.railway.app',]
 # Application definition
 
 INSTALLED_APPS = [
